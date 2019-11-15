ce4d3f4bef16658974824b298b0de025c0e084fd
hogan chen <hogan.chen@ymail.com>
2019-11-15 18:45:56 +0800

    markdown update

--- c/sizeof.c
+++ c/sizeof.c
@@ -3,6 +3,7 @@
 
 int main() {
     int a[10];
+    char b[16];
 
     printf("sizeof(int) = %lu\n", sizeof(int));
     printf("sizeof(long) = %lu\n", sizeof(long));
@@ -18,6 +19,14 @@ int main() {
 
     printf("sizeof(a) = %lu\n", sizeof(a));
     printf("sizeof(&a) = %lu\n", sizeof(&a));
+
+    printf("a address: %p\n", a);
+    printf("&a address: %p\n", &a);
+    printf("&a+1 address: %p\n", &a+1);
+
+    printf("b address: %p\n", b);
+    printf("&b address: %p\n", &b);
+    printf("&b+1 address: %p\n", &b+1);
 
     return 0;
 }
