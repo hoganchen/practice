3bee3d621145edf8927b6c94bfc400178509842a
hogan chen <hogan.chen@ymail.com>
2019-12-04 11:35:54 +0800

    markdown update

---
+++ python/basic/python_funny_template.py
@@ -0,0 +1,78 @@
+# -*- coding:utf-8 -*-
+
+"""
+@Author:        hogan.chen@ymail.com
+@Create Date:   2019-01-12
+
+                      d*##$.
+ zP'''''$e.           $'    $o
+4$       '$          $'      $
+'$        '$        J$       $F
+ 'b        $k       $>       $
+  $k        $r     J$       d$
+  '$         $     $'       $~
+   '$        '$   '$E       $
+    $         $L   $'      $F ...
+     $.       4B   $      $$$*'''*b
+     '$        $.  $$     $$      $F
+      '$       R$  $F     $'      $
+       $k      ?$ u*     dF      .$
+       ^$.      $$'     z$      u$$$$e
+        #$b             $E.dW@e$'    ?$
+         #$           .o$$# d$$$$c    ?F
+          $      .d$$#' . zo$>   #$r .uF
+          $L .u$*'      $&$$$k   .$$d$$F
+           $$'            ''^'$$$P'$P9$
+          JP              .o$$$$u:$P $$
+          $          ..ue$'      ''  $'
+         d$          $F              $
+         $$     ....udE             4B
+          #$    ''''` $r            @$
+           ^$L        '$            $F
+             RN        4N           $
+              *$b                  d$
+               $$k                 $F
+               $$b                $F
+                 $''               $F
+                 '$                $
+                  $L               $
+                  '$               $
+                   $               $
+
+"""
+
+import time
+import logging
+import datetime
+
+# log level
+LOGGING_LEVEL = logging.INFO
+
+
+def logging_config(logging_level):
+    # log_format = "%(asctime)s - %(levelname)s - %(message)s"
+    # log_format = "%(asctime)s [line: %(lineno)d] - %(levelname)s - %(message)s"
+    # log_format = "[File: %(filename)s line: %(lineno)d] - %(levelname)s - %(message)s"
+    # log_format = "[%(asctime)s - [File: %(filename)s line: %(lineno)d] - %(levelname)s]: %(message)s"
+    # log_format = "[Func: %(funcName)s - Line: %(lineno)d - Level: %(levelname)s]: %(message)s"
+    # log_format = "[Datetime: %(asctime)s - Line: %(lineno)d - Level: %(levelname)s]: %(message)s"
+    # log_format = "[Time: %(asctime)s - Func: %(funcName)s - Line: %(lineno)d - Level: %(levelname)s]: %(message)s"
+
+    log_format = "[Func: %(funcName)s - Line: %(lineno)d - Level: %(levelname)s]: %(message)s"
+    logging.basicConfig(level=logging_level, format=log_format)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    logging_config(LOGGING_LEVEL)
+    print('Script start execution at {}\n'.format(str(datetime.datetime.now())))
+
+    time_start = time.time()
+
+    main()
+
+    print('\nTotal elapsed time: {} seconds'.format(time.time() - time_start))
+    print('Script end execution at {}'.format(datetime.datetime.now()))
