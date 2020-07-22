6daa2e02061f3bf208dd882bb7f77800fc9aa287
hoganchen <hogan.chen@ymail.com>
2020-07-22 16:43:32 +0800

    Update openpyxl_use.py

--- python/openpyxl/openpyxl_use.py
+++ python/openpyxl/openpyxl_use.py
@@ -1,8 +1,9 @@
+# -*- coding:utf-8 -*-
 import os
 import re
 import time
 import logging
-import datetime
+# import datetime
 import argparse
 import openpyxl
 import openpyxl.styles
@@ -11,6 +12,8 @@ REGION_COLUMN_NUM = 8
 REGION_SIZE_INDEX = 2
 REGION_TYPE_INDEX = 3
 REGION_PAD_STRING = 'PAD'
+
+BLE_STACK_SIZE = 16 * 1024
 
 LIB_FILE_LIST = ('ble_sdk_gr551x_d.lib', 'ble_sdk_gr551x_c.lib', 'ble_sdk_gr551x.lib')
 OTHERS_EXTEND_LIST = ('main.o',)
@@ -99,13 +102,18 @@ def parse_map_file(file_name):
                                     line_str)
                 flash_obj = re.match(r'^\s+Execution\s+Region\s+FLASH_CODE\s+\(Exec\s+base:\s+(0x[0-9a-f]{8}),'
                                      r'\s+Load\s+base:\s+(0x[0-9a-f]{8})', line_str)
+                ram_rw_obj = re.match(r'^\s+\d+\s+\d+\s+\d+\s+(\d+)\s+\d+\s+\d+\s+ELF\s+'
+                                      r'Image\s+Totals\s*\(compressed\)', line_str)
 
                 if size_obj:
-                    info_dict['size'] = size_obj.group(1)
+                    info_dict['rom_size'] = size_obj.group(1)
 
                 if flash_obj:
                     info_dict['exec_base'] = flash_obj.group(1)
                     info_dict['load_base'] = flash_obj.group(2)
+
+                if ram_rw_obj:
+                    info_dict['ram_rw_elf'] = ram_rw_obj.group(1)
             else:
                 break
 
@@ -145,9 +153,9 @@ def gen_excel_report(map_file, args_param):
     for flash_dict in region_dict['FLASH_CODE']:
         logging.debug('flash_dict: {}'.format(flash_dict))
         logging.debug('os.path.splitext(os.path.basename(flash_dict["name"]))[0]: {}'.
-                     format(os.path.splitext(os.path.basename(flash_dict['name']))[0]))
+                      format(os.path.splitext(os.path.basename(flash_dict['name']))[0]))
         logging.debug('driver_file_dict.get(os.path.splitext(os.path.basename(flash_dict["name"]))[0]): {}'.
-                     format(driver_file_dict.get(os.path.splitext(os.path.basename(flash_dict['name']))[0])))
+                      format(driver_file_dict.get(os.path.splitext(os.path.basename(flash_dict['name']))[0])))
 
         if -1 != flash_dict['name'].find('.lib'):
             flash_use_dict['lib'] += flash_dict['size']
@@ -198,13 +206,15 @@ def gen_excel_report(map_file, args_param):
         else:
             ram_code_use_dict['user'] += ram_code_dict['size']
 
-    logging.info('\n\nflash_use_dict: {}'.format(flash_use_dict))
-    logging.info('\n\nram_zi_use_dict: {}'.format(ram_zi_use_dict))
-    logging.info('\n\nram_rw_use_dict: {}'.format(ram_rw_use_dict))
-    logging.info('\n\nram_code_use_dict: {}'.format(ram_code_use_dict))
+    logging.debug('\n\nflash_use_dict: {}'.format(flash_use_dict))
+    logging.debug('\n\nram_zi_use_dict: {}'.format(ram_zi_use_dict))
+    logging.debug('\n\nram_rw_use_dict: {}'.format(ram_rw_use_dict))
+    logging.debug('\n\nram_code_use_dict: {}'.format(ram_code_use_dict))
 
+    print('Start to generate the parse report...', flush=True)
     wb = openpyxl.Workbook()
     ws = wb.active
+    ws.title = 'Parser_Result'
 
     ws.merge_cells('A1:A4')
     ws['A1'] = 'Bin Size'
@@ -214,7 +224,7 @@ def gen_excel_report(map_file, args_param):
     ws['C1'] = 'Run Address'
 
     ws.merge_cells('D1:H1')
-    ws['D1'] = 'Flash'
+    ws['D1'] = 'Flash (before compression)(bytes)'
     ws.merge_cells('D2:D4')
     ws['D2'] = 'All'
     ws.merge_cells('E2:G2')
@@ -228,8 +238,8 @@ def gen_excel_report(map_file, args_param):
     ws.merge_cells('H2:H4')
     ws['H2'] = 'User'
 
-    ws.merge_cells('I1:X1')
-    ws['I1'] = 'RAM'
+    ws.merge_cells('I1:Z1')
+    ws['I1'] = 'RAM(Bytes)'
     ws.merge_cells('I2:I4')
     ws['I2'] = 'All'
 
@@ -269,10 +279,10 @@ def gen_excel_report(map_file, args_param):
     ws['V4'] = 'Driver'
     ws['W4'] = 'Other'
 
-    ws.merge_cells('Y1:Y4')
-    ws['Y1'] = 'BLE Stack'
-    ws.merge_cells('Z1:Z4')
-    ws['Z1'] = 'Call Stack'
+    ws.merge_cells('Y2:Y4')
+    ws['Y2'] = 'RAM_USED_IN_ROM'
+    ws.merge_cells('Z2:Z4')
+    ws['Z2'] = 'Call Stack'
 
     bd = openpyxl.styles.Side(border_style='thin')
     for cell_column in [chr(i) for i in range(65, 91)]:
@@ -284,140 +294,160 @@ def gen_excel_report(map_file, args_param):
             ws['{}{}'.format(cell_column, cell_row)].alignment = \
                 openpyxl.styles.Alignment(horizontal='center', vertical='center', wrapText=True)
 
-    ws['A5'] = '{} bytes'.format(info_dict['size'])
-    ws['B5'] = info_dict['load_base']
-    ws['C5'] = info_dict['exec_base']
-    ws['D5'] = flash_use_dict['lib'] + flash_use_dict['driver'] + flash_use_dict['other'] + flash_use_dict['user']
-    ws['E5'] = flash_use_dict['lib']
-    ws['F5'] = flash_use_dict['driver']
-    ws['G5'] = flash_use_dict['other']
-    ws['H5'] = flash_use_dict['user']
+    ws['A5'] = '{} bytes'.format(info_dict['rom_size'])
+    if info_dict.get('load_base') is not None:
+        ws['B5'] = info_dict['load_base']
+    if info_dict.get('exec_base') is not None:
+        ws['C5'] = info_dict['exec_base']
+
+    ws['D5'] = flash_use_dict['lib'] + flash_use_dict['driver'] + flash_use_dict['other'] + flash_use_dict['user'] + \
+        ram_rw_use_dict['lib'] + ram_rw_use_dict['driver'] + ram_rw_use_dict['other'] + ram_rw_use_dict['user'] +\
+        ram_code_use_dict['lib'] + ram_code_use_dict['driver'] + ram_code_use_dict['other'] + \
+        ram_code_use_dict['user']
+    ws['E5'] = flash_use_dict['lib'] + ram_rw_use_dict['lib'] + ram_code_use_dict['lib']
+    ws['F5'] = flash_use_dict['driver'] + ram_rw_use_dict['driver'] + ram_code_use_dict['driver']
+    ws['G5'] = flash_use_dict['other'] + ram_rw_use_dict['other'] + ram_code_use_dict['other']
+    ws['H5'] = flash_use_dict['user'] + ram_rw_use_dict['user'] + ram_code_use_dict['user']
+
     ws['I5'] = ram_zi_use_dict['lib'] + ram_zi_use_dict['driver'] + ram_zi_use_dict['other'] + \
         ram_zi_use_dict['user'] + ram_rw_use_dict['lib'] + ram_rw_use_dict['driver'] + \
         ram_rw_use_dict['other'] + ram_rw_use_dict['user'] + ram_code_use_dict['lib'] + \
-        ram_code_use_dict['driver'] + ram_code_use_dict['other'] + ram_code_use_dict['user']
+        ram_code_use_dict['driver'] + ram_code_use_dict['other'] + ram_code_use_dict['user'] + BLE_STACK_SIZE + \
+        region_dict['ARM_LIB_STACKHEAP'][0]['size']
     ws['J5'] = ram_zi_use_dict['lib'] + ram_zi_use_dict['driver'] + ram_zi_use_dict['other'] + ram_zi_use_dict['user']
     ws['K5'] = ram_zi_use_dict['lib']
     ws['L5'] = ram_zi_use_dict['driver']
     ws['M5'] = ram_zi_use_dict['other']
     ws['N5'] = ram_zi_use_dict['user']
+
     ws['O5'] = ram_rw_use_dict['lib'] + ram_rw_use_dict['driver'] + ram_rw_use_dict['other'] + ram_rw_use_dict['user']
     ws['P5'] = ram_rw_use_dict['lib']
     ws['Q5'] = ram_rw_use_dict['driver']
     ws['R5'] = ram_rw_use_dict['other']
     ws['S5'] = ram_rw_use_dict['user']
+
     ws['T5'] = ram_code_use_dict['lib'] + ram_code_use_dict['driver'] + ram_code_use_dict['other'] + \
         ram_code_use_dict['user']
     ws['U5'] = ram_code_use_dict['lib']
     ws['V5'] = ram_code_use_dict['driver']
     ws['W5'] = ram_code_use_dict['other']
     ws['X5'] = ram_code_use_dict['user']
-    ws['Y5'] = '16 KB'
-    ws['Z5'] = '{} bytes'.format(region_dict['ARM_LIB_STACKHEAP'][0]['size'])
+    ws['Y5'] = BLE_STACK_SIZE
+    ws['Z5'] = region_dict['ARM_LIB_STACKHEAP'][0]['size']
 
-    ws = wb.create_sheet()
-    ws.column_dimensions['A'].width = 60
-    ws.column_dimensions['B'].width = 10
-    ws.column_dimensions['C'].width = 10
-    current_column = 1
+    if 'true' == args_param.debug_flag.lower():
+        ws = wb.create_sheet()
+        ws.column_dimensions['A'].width = 60
+        ws.column_dimensions['B'].width = 15
+        ws.column_dimensions['C'].width = 15
+        current_column = 1
 
-    ws['A1'] = 'File Name'
-    ws['B1'] = 'Type'
-    ws['C1'] = 'Size'
+        ws['A1'] = 'File Name'
+        ws['B1'] = 'Type'
+        ws['C1'] = 'Size(Bytes)'
 
-    flash_dict_list = region_dict['FLASH_CODE'] + region_dict['RAM_RW']
+        flash_dict_list = region_dict['FLASH_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_CODE']
 
-    flash_dict_list.sort(key=lambda x: x.get('size'), reverse=True)
-    logging.debug('flash_dict_list: \n{}'.format(flash_dict_list))
-    # sorted_list = sorted(test_result_info.items(), key=lambda items: items[1]['rerun_time'], reverse=True)
+        flash_dict_list.sort(key=lambda x: x.get('size'), reverse=True)
+        logging.debug('flash_dict_list: \n{}'.format(flash_dict_list))
+        # sorted_list = sorted(test_result_info.items(), key=lambda items: items[1]['rerun_time'], reverse=True)
 
-    for flash_dict in flash_dict_list:
-        current_column += 1
-        ws['A{}'.format(current_column)] = flash_dict.get('name')
-        ws['B{}'.format(current_column)] = flash_dict.get('type')
-        ws['C{}'.format(current_column)] = flash_dict.get('size')
+        for flash_dict in flash_dict_list:
+            current_column += 1
+            ws['A{}'.format(current_column)] = flash_dict.get('name')
+            ws['B{}'.format(current_column)] = flash_dict.get('type')
+            ws['C{}'.format(current_column)] = flash_dict.get('size')
 
-    ws = wb.create_sheet(title='flash_size')
-    ws.column_dimensions['A'].width = 60
-    ws.column_dimensions['B'].width = 10
-    current_column = 1
+    if 'true' == args_param.flash_flag.lower():
+        ws = wb.create_sheet(title='Flash_Use')
+        ws.column_dimensions['A'].width = 60
+        ws.column_dimensions['B'].width = 15
+        current_column = 1
 
-    ws['A1'] = 'File Name'
-    ws['B1'] = 'Size'
+        ws['A1'] = 'File Name'
+        ws['B1'] = 'Size(Bytes)'
 
-    flash_dict_list = region_dict['FLASH_CODE'] + region_dict['RAM_RW']
-    flash_size_dict = {}
+        flash_dict_list = region_dict['FLASH_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_CODE']
+        flash_size_dict = {}
 
-    for flash_dict in flash_dict_list:
-        if flash_size_dict.get(flash_dict['name']) is None:
-            flash_size_dict[flash_dict['name']] = flash_dict.get('size')
-        else:
-            flash_size_dict[flash_dict['name']] += flash_dict.get('size')
+        for flash_dict in flash_dict_list:
+            if flash_size_dict.get(flash_dict['name']) is None:
+                flash_size_dict[flash_dict['name']] = flash_dict.get('size')
+            else:
+                flash_size_dict[flash_dict['name']] += flash_dict.get('size')
 
-    flash_sorted_list = sorted(flash_size_dict.items(), key=lambda items: items[1], reverse=True)
+        flash_sorted_list = sorted(flash_size_dict.items(), key=lambda items: items[1], reverse=True)
 
-    for flash_tuple in flash_sorted_list:
-        current_column += 1
-        ws['A{}'.format(current_column)] = flash_tuple[0]
-        ws['B{}'.format(current_column)] = flash_tuple[1]
+        for flash_tuple in flash_sorted_list:
+            current_column += 1
+            ws['A{}'.format(current_column)] = flash_tuple[0]
+            ws['B{}'.format(current_column)] = flash_tuple[1]
 
-    ws = wb.create_sheet()
-    ws.column_dimensions['A'].width = 60
-    ws.column_dimensions['B'].width = 10
-    ws.column_dimensions['C'].width = 10
-    current_column = 1
+    if 'true' == args_param.debug_flag.lower():
+        ws = wb.create_sheet()
+        ws.column_dimensions['A'].width = 60
+        ws.column_dimensions['B'].width = 15
+        ws.column_dimensions['C'].width = 15
+        current_column = 1
 
-    ws['A1'] = 'File Name'
-    ws['B1'] = 'Type'
-    ws['C1'] = 'Size'
+        ws['A1'] = 'File Name'
+        ws['B1'] = 'Type'
+        ws['C1'] = 'Size(Bytes)'
 
-    ram_dict_list = region_dict['RAM_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_ZI']
-    ram_dict_list.sort(key=lambda x: x.get('size'), reverse=True)
-    logging.debug('ram_dict_list: \n{}'.format(ram_dict_list))
+        ram_dict_list = region_dict['RAM_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_ZI']
+        ram_dict_list.sort(key=lambda x: x.get('size'), reverse=True)
+        logging.debug('ram_dict_list: \n{}'.format(ram_dict_list))
 
-    for ram_dict in ram_dict_list:
-        current_column += 1
-        ws['A{}'.format(current_column)] = ram_dict.get('name')
-        ws['B{}'.format(current_column)] = ram_dict.get('type')
-        ws['C{}'.format(current_column)] = ram_dict.get('size')
+        for ram_dict in ram_dict_list:
+            current_column += 1
+            ws['A{}'.format(current_column)] = ram_dict.get('name')
+            ws['B{}'.format(current_column)] = ram_dict.get('type')
+            ws['C{}'.format(current_column)] = ram_dict.get('size')
 
-    ws = wb.create_sheet(title='ram_size')
-    ws.column_dimensions['A'].width = 60
-    ws.column_dimensions['B'].width = 10
-    current_column = 1
+    if 'true' == args_param.ram_flag.lower():
+        ws = wb.create_sheet(title='RAM_Use')
+        ws.column_dimensions['A'].width = 60
+        ws.column_dimensions['B'].width = 15
+        current_column = 1
 
-    ws['A1'] = 'File Name'
-    ws['B1'] = 'Size'
+        ws['A1'] = 'File Name'
+        ws['B1'] = 'Size(Bytes)'
 
-    ram_dict_list = region_dict['RAM_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_ZI']
-    ram_size_dict = {}
+        ram_dict_list = region_dict['RAM_CODE'] + region_dict['RAM_RW'] + region_dict['RAM_ZI']
+        ram_size_dict = {}
 
-    for ram_dict in ram_dict_list:
-        if ram_size_dict.get(ram_dict['name']) is None:
-            ram_size_dict[ram_dict['name']] = ram_dict.get('size')
-        else:
-            ram_size_dict[ram_dict['name']] += ram_dict.get('size')
+        for ram_dict in ram_dict_list:
+            if ram_size_dict.get(ram_dict['name']) is None:
+                ram_size_dict[ram_dict['name']] = ram_dict.get('size')
+            else:
+                ram_size_dict[ram_dict['name']] += ram_dict.get('size')
 
-    ram_sorted_list = sorted(ram_size_dict.items(), key=lambda items: items[1], reverse=True)
+        ram_sorted_list = sorted(ram_size_dict.items(), key=lambda items: items[1], reverse=True)
 
-    for ram_tuple in ram_sorted_list:
-        current_column += 1
-        ws['A{}'.format(current_column)] = ram_tuple[0]
-        ws['B{}'.format(current_column)] = ram_tuple[1]
+        for ram_tuple in ram_sorted_list:
+            current_column += 1
+            ws['A{}'.format(current_column)] = ram_tuple[0]
+            ws['B{}'.format(current_column)] = ram_tuple[1]
 
-    wb.save('{}.xlsx'.format(os.path.basename(os.path.splitext(map_file)[0])))
+    wb.save('{}.xlsx'.format(os.path.join(args_param.map_path, os.path.basename(os.path.splitext(map_file)[0]))))
 
 
 def get_command_line_parameter():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-p', '--path', action='store', dest='map_path', default='.\\',
+    parser.add_argument('-p', '--path', action='store', dest='map_path', default='.\\build',
                         help='The path of map file')
-    parser.add_argument('-l', '--lib', nargs='+', action='store', dest='lib_files',
-                        default=['ble_sdk_gr551x_d.lib', 'ble_sdk_gr551x_c.lib', 'ble_sdk_gr551x.lib'],
-                        help='The library file list')
-    parser.add_argument('-s', '--sdk', action='store', dest='sdk_base', default='.\\',
-                        help='The driver path')
+    # parser.add_argument('-l', '--lib', nargs='+', action='store', dest='lib_files',
+    #                     default=['ble_sdk_gr551x_d.lib', 'ble_sdk_gr551x_c.lib', 'ble_sdk_gr551x.lib'],
+    #                     help='The library file list')
+    parser.add_argument('-s', '--sdk', action='store', dest='sdk_base', default='..\\..\\..\\..\\..\\',
+                        help='The sdk base path')
+    parser.add_argument('-d', '--debug', action='store', dest='debug_flag', default='False',
+                        help='The debug flag')
+    parser.add_argument('-f', '--flash', action='store', dest='flash_flag', default='False',
+                        help='The flash use flag')
+    parser.add_argument('-r', '--ram', action='store', dest='ram_flag', default='False',
+                        help='The ram use flag')
 
     args_param = parser.parse_args()
 
@@ -426,19 +456,24 @@ def get_command_line_parameter():
 
 def main():
     args_param = get_command_line_parameter()
+
+    if 'true' == args_param.debug_flag.lower():
+        logging_config(logging.DEBUG)
+    else:
+        logging_config(logging.INFO)
+
     map_file_list = get_all_files_with_suffix_by_os_walk(args_param.map_path, MAP_FILE_SUFFIX)
 
     for map_file in map_file_list:
+        print('Start to parse {} file...'.format(os.path.basename(map_file)), flush=True)
         gen_excel_report(map_file, args_param)
 
 
 if __name__ == "__main__":
-    print("Script start execution at {}".format(str(datetime.datetime.now())))
+    # print("Script start execution at {}".format(str(datetime.datetime.now())))
 
     time_start = time.time()
-    logging_config(LOGGING_LEVEL)
     main()
 
-    print("\n\nTotal Elapsed Time: {} seconds".format(time.time() - time_start))
-    print("\nScript end execution at {}".format(datetime.datetime.now()))
-
+    # print("\n\nTotal Elapsed Time: {} seconds".format(time.time() - time_start))
+    # print("\nScript end execution at {}".format(datetime.datetime.now()))
