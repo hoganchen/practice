921b2d5e16d658c1f70ea7725994ec8a017a3ac8
hogan chen <hogan.chen@ymail.com>
2021-06-11 18:08:18 +0800

    markdown update

---
+++ go/golang_design_implementation/04_basic/02_01_interface.go
@@ -0,0 +1,44 @@
+/*
+@Author:        hogan.chen@ymail.com
+@Create Date:   2020-11-11
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type RPCError struct {
+	Code int64
+	Message string
+}
+
+func main() {
+	start := time.Now()
+	fmt.Printf("Program start execution at %s\n\n", start.Format("2006-01-02 15:04:05"))
+
+	var rpcErr error = NewRPCError(400, "unknown err") // typecheck1
+	err := AsErr(rpcErr) // typecheck2
+	println(err)
+
+	elapsed := time.Since(start)
+	fmt.Printf("\nProgram end execution at %s\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("Total elapsed time: %s\n", elapsed)
+}
+
+/*
+cannot use RPCError literal (type *RPCError) as type error in return argument:
+	*RPCError does not implement error (missing Error method)
+*/
+func NewRPCError(code int64, msg string) error {
+	return &RPCError{ // typecheck3
+		Code:    code,
+		Message: msg,
+	}
+}
+
+func AsErr(err error) error {
+	return err
+}
---
+++ go/golang_design_implementation/04_basic/02_02_interface.go
@@ -0,0 +1,29 @@
+/*
+@Author:        hogan.chen@ymail.com
+@Create Date:   2020-11-11
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func Print(v interface{}) {
+	println(v)
+}
+
+func main() {
+	start := time.Now()
+	fmt.Printf("Program start execution at %s\n\n", start.Format("2006-01-02 15:04:05"))
+
+	type test struct{}
+	v := test{}
+	fmt.Printf("type(v): %T\n", v)
+	Print(v)
+
+	elapsed := time.Since(start)
+	fmt.Printf("\nProgram end execution at %s\n", time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Printf("Total elapsed time: %s\n", elapsed)
+}
