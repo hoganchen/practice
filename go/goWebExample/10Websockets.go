557ff1ed5504c22f35829a59e93fd0d2aa8ecb2b
hoganchen <hogan.chen@ymail.com>
2022-02-25 17:31:33 +0800

    Create 10Websockets.go

---
+++ go/goWebExample/10Websockets.go
@@ -0,0 +1,50 @@
+// websockets.go
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+/*
+这个例子将展示如何使用Websockets。我们将建立一个简单的server,它发回我们发给它所有的内容。这里我们用go get来下载 gorilla/websocket :
+
+$ go get github.com/gorilla/websocket
+
+接下来，我们所有的应用都将使用这个库.
+*/
+// http://books.studygolang.com/gowebexamples/websockets/
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
