a160d5ef18ea9c242cc9b8bf32b102d917b801cf
hoganchen <hogan.chen@ymail.com>
2022-08-02 19:43:50 +0800

    Create utility_01.py

---
+++ python/basic/run_batch/utility_01.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+"""
+@Author:        hogan.chen@ymail.com
+@Create Date:   10/01/21
+"""
+
+
+def main():
+    # return 0
+    return True
+
+
+if __name__ == '__main__':
+    ret_val = main()
+
+    print('run first utility script, ret_val: {}'.format(ret_val))
+
+    if isinstance(ret_val, bool):
+        exit(int(not ret_val))
+    elif isinstance(ret_val, int):
+        exit(ret_val)
+    else:
+        exit(0)
