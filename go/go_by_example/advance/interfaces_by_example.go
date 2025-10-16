b587489aced0b262f38471b96d3d14c3ad940574
Hogan Chen <hogan.chen@ymail.com>
2025-10-16 16:48:51 +0800

    markdown update

--- go/go_by_example/advance/interfaces_by_example.go
+++ go/go_by_example/advance/interfaces_by_example.go
@@ -42,6 +42,58 @@ func measure(g geometry) {
 	fmt.Println(g.perim())
 }
 
+// 空接口，接受任意类型参数的函数
+func printAnything(v interface{}) {
+	fmt.Printf("Received value: %v, Type: %T\n", v, v)
+}
+
+// 空接口，接受任意类型参数的函数
+func processValue(i interface{}) {
+	// // 类型断言
+	// if s, ok := i.(string); ok {
+	// 	fmt.Println("It's a string:", s)
+	// } else if n, ok := i.(int); ok {
+	// 	fmt.Println("It's an int:", n)
+	// } else {
+	// 	fmt.Printf("Unhandled type: %T\n", i)
+	// }
+
+	// 或者使用类型选择（更清晰）
+	switch v := i.(type) {
+	case string:
+		fmt.Printf("String: %s\n", v)
+	case int:
+		fmt.Printf("Int: %d\n", v)
+	case float64:
+		fmt.Printf("Float64: %f\n", v)
+	case []int:
+		fmt.Printf("Slice of int: %v\n", v)
+	case rect:
+		fmt.Printf("Rectangle: width=%f, height=%f\n", v.width, v.height)
+	case circle:
+		fmt.Printf("Circle: radius=%f\n", v.radius)
+	default:
+		fmt.Printf("Unexpected type: %T\n", v)
+	}
+}
+
+func mainPrint() {
+	// 这条语句的作用是创建了一个名为 iSlice的空接口类型变量，并用一个切片来初始化它。
+	// 这个切片的特殊之处在于，它的每个元素也是空接口类型，并且被赋予了不同类型的值，从而形成了一个可以容纳任意类型数据的 ​​“异构容器”
+	var iSlice interface{} = []interface{}{42, "hello", 3.14, []int{1, 2, 3}, rect{width: 5, height: 10}, circle{radius: 7}}
+
+	for _, v := range iSlice.([]interface{}) {
+		processValue(v)
+		printAnything(v)
+	}
+
+	// printAnything(42)                          // int
+	// printAnything(42.12)                       // float64
+	// printAnything("hello")                     // string
+	// printAnything([]int{1, 2, 3})              // slice
+	// printAnything(rect{width: 10, height: 10}) // slice
+}
+
 func main() {
 	r := rect{width: 3, height: 4}
 	c := circle{radius: 5}
@@ -55,4 +107,6 @@ func main() {
 	measure(i)
 	i = c
 	measure(i)
+
+	mainPrint()
 }
--- go/tour.go-zh.org/02_basics/15_constants.go
+++ go/tour.go-zh.org/02_basics/15_constants.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+/*
+常量的类型
+常量可以有类型，也可以无类型。无类型常量在使用时会根据上下文确定类型。
+
+​有类型常量​：
+const Pi float64 = 3.14159
+
+​无类型常量​：
+const Pi = 3.14159  // 无类型浮点常量
+const value = 5     // 无类型整数常量
+
+无类型常量在赋值给变量时，如果变量类型与常量值兼容，则会将常量转换为该类型。例如，可以将一个无类型整数常量赋值给int、float64等类型的变量。
+*/
 const Pi = 3.14
+const typePi float64 = 3.14
 
 /*
 
@@ -19,6 +33,8 @@ func main() {
 	const World = "世界"
 	fmt.Println("Hello", World)
 	fmt.Println("Happy", Pi, "Day")
+	fmt.Println("Pi:", Pi)
+	fmt.Println("typePi:", typePi)
 
 	const Truth = true
 	fmt.Println("Go rules?", Truth)
